0571424951a9a14dd9689a87e2e20e24e6948938
Fan Xia <fxia@salesforce.com>
2021-03-09 08:34:23 -0800

    implement edit post

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 export const SinglePostPage = ({ match }) => {
     const { postId } = match.params;
 
-    const post = useSelector(state => state.posts.find(post => post.id === postId));
+    const post = useSelector((state) =>
+        state.posts.find((post) => post.id === postId)
+    );
 
     if (!post) {
         return (
             <section>
                 <h2>Post not found</h2>
             </section>
-        )
+        );
     }
 
     return (
@@ -19,7 +22,10 @@ export const SinglePostPage = ({ match }) => {
             <article className="post">
                 <h2>{post.title}</h2>
                 <p className="post-content">{post.content}</p>
+                <Link to={`/editPost/${post.id}`} className="button">
+                    Edit Post
+                </Link>
             </article>
         </section>
     );
-}
+};
